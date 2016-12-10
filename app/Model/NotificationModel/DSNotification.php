14c293a06a482ef40bf6d6f4745dc6bf1365b47e
highjump <lixiang901112@163.com>
2016-12-10 13:48:57 +0800

    Revised code for notification

--- app/Model/NotificationModel/DSNotification.php
+++ app/Model/NotificationModel/DSNotification.php
@@ -6,11 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 use DateTime;
 use DateTimeZone;
 
-class DSNotification extends Model
+class DSNotification extends BaseNotification
 {
-    const READ_STATUS = 1;
-    const UNREAD_STATUS = 0;
-
     const NOTIFICATION_PRODUCE = 1;
     
     protected $table = "dsnotifications";
