d858a6ebf4aa1710d40c3240e6fefaf81921e602
thestrong21 <thestrong21@yahoo.com>
2016-12-13 21:04:21 +0800

    Fix for xinxizhongxin.blade.php

--- resources/views/weixin/xinxizhongxin.blade.php
+++ resources/views/weixin/xinxizhongxin.blade.php
@@ -1,7 +1,12 @@
 @extends('weixin.layout.master')
 @section('title','消息中心')
 @section('css')
-
+    <style>
+        li a.cont {
+            text-indent: 0px;
+            padding-left: 10px;
+        }
+    </style>
 @endsection
 @section('content')
 
@@ -15,7 +20,7 @@
             <p class="no_data"> 没有消息 </p>
         @else
             @foreach($reviews as $rw)
-                <li><a class="col-xs-9" href="#">{{$rw->content}}</a><span class="col-xs-3">{{$rw->created_at}}</span></li>
+                <li><a class="col-xs-9 cont" href="#">{{$rw->content}}</a><span class="col-xs-3">{{$rw->created_at}}</span></li>
             @endforeach
         @endif
     </ul>
