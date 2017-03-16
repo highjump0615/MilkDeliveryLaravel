ca86188354dac63dde98c9f76e608119d6bed46a
highjump <lixiang901112@163.com>
2017-03-17 07:28:43 +0800

    Removed unnecessary get for performance

--- app/Http/Controllers/DSProductionPlanCtrl.php
+++ app/Http/Controllers/DSProductionPlanCtrl.php
@@ -1053,7 +1053,6 @@ sum(group_sale * settle_product_price) as group_amount,sum(channel_sale * settle
             ->where('produce_end_at',$current_date_str)
             ->where('product_id',$product_id)
             ->where('status',DSProductionPlan::DSPRODUCTION_PRODUCE_SENT)
-            ->get()
             ->first();
 
         if ($dsplan != null){
@@ -1081,7 +1080,6 @@ sum(group_sale * settle_product_price) as group_amount,sum(channel_sale * settle
             $bottle_refund = DSBottleRefund::where('time',$current_date_str)
                 ->where('station_id',$current_station_id)
                 ->where('bottle_type',$object_type)
-                ->get()
                 ->first();
 
             if($bottle_refund != null){
@@ -1103,7 +1101,6 @@ sum(group_sale * settle_product_price) as group_amount,sum(channel_sale * settle
             $box_refund = DSBoxRefund::where('time',$current_date_str)
                 ->where('station_id',$current_station_id)
                 ->where('box_type',$object_type)
-                ->get()
                 ->first();
 
             if($box_refund != null){
