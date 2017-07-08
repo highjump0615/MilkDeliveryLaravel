eba86010fe84cd36fa9ef3a9c0a9339c89fa80f0
highjump <lixiang901112@163.com>
2017-07-09 00:47:46 +0800

    Updated some view format

--- resources/views/gongchang/jichuxinxi/naizhan/peisongfanwei-chakanbianji.blade.php
+++ resources/views/gongchang/jichuxinxi/naizhan/peisongfanwei-chakanbianji.blade.php
@@ -46,9 +46,9 @@
                     <table id="delivery_area_table" class="footable table table-bordered" data-page-size="10">
                         <thead>
                         <tr>
-                            <th data-sort-ignore="true">街道</th>
+                            <th style="width:110px" data-sort-ignore="true">街道</th>
                             <th data-sort-ignore="true">配送范围</th>
-                            <th data-sort-ignore="true">操作</th>
+                            <th style="width:130px" data-sort-ignore="true">操作</th>
                         </tr>
                         </thead>
                         <tbody>
