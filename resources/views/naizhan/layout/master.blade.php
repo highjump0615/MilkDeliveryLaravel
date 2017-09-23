3af29d84424e5a58a39877dcfdce21c70e60f8f9
highjump <lixiang901112@163.com>
2017-09-24 03:39:27 +0800

    Improved style for side menu

--- resources/views/naizhan/layout/master.blade.php
+++ resources/views/naizhan/layout/master.blade.php
@@ -21,7 +21,7 @@
     <link href="<?=asset('css/plugins/iCheck/custom.css') ?>" rel="stylesheet">
 
     <link href="<?=asset('css/animate.css') ?>" rel="stylesheet">
-    <link href="<?=asset('css/style.css') ?>" rel="stylesheet">
+    <link href="<?=asset('css/style.css?170923') ?>" rel="stylesheet">
     <link href="<?=asset('css/custom.css') ?>" rel="stylesheet">
 
     @yield('css')
